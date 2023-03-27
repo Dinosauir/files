6eacda80b07d1b59396904e7ef9b9f5c6114faee
Sebastian <sebastianvlasceanu@icloud.com>
2023-03-27 12:39:21 +0300

    package init

---
+++ src/Package.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Abacus\Files;
+
+class Package
+{
+    public const DB_PREFIX = 'abacus';
+
+    public const PATH = '%s/%s.%s';
+
+    public const FOLDER_NAME = 'files';
+
+    public const DB_PREFIX = 'abacus';
+}
---
+++ src/Persistence/Concerns/HasFile.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Abacus\Files\Persistence\Concerns;
+
+use Abacus\Files\Persistence\File;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+trait HasFile
+{
+    final public function files(): MorphOne
+    {
+        return $this->morphOne(File::class, 'fileable');
+    }
+}
